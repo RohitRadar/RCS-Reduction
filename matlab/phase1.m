7e46bd18b81ae453b0da58301695abfa5bff9d46
RohitD <rohitradar@gmail.com>
2021-12-28 15:07:33 +0530

    Added Phase Folder

--- matlab/phase1.m
+++ matlab/phase1.m
@@ -5,7 +5,7 @@ close all;
 file = fopen("pics/readme.md",'w');
 for i=1:20:5001
     fprintf(file,"#### iteration"+num2str(i)+"\n");
-    fprintf(file,"<img src='https://github.com/RohitRadar/RCS-Reduction/tree/main/matlab/pics/iter"+num2str(i)+".jpg' width='100' height='100'>"+"\n");
+    fprintf(file,"<img src='https://github.com/RohitRadar/RCS-Reduction/blob/main/matlab/pics/iter"+num2str(i)+".jpg' width='100' height='100'>"+"\n");
     %fprintf(file,"![image](/matlab/pics/iter"+num2str(i)+".jpg)"+"\n");
 end
 fclose(file);
