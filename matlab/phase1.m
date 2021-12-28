2ba953d94bfa80588cc881e20671c4197fb6ab8e
RohitD <rohitradar@gmail.com>
2021-12-28 14:59:10 +0530

    Added Phase Folder

--- matlab/phase1.m
+++ matlab/phase1.m
@@ -5,7 +5,7 @@ close all;
 file = fopen("pics/readme.md",'w');
 for i=1:20:5001
     fprintf(file,"#### iteration"+num2str(i)+"\n");
-    fprintf(file,"![image](/matlab/pics/iter"+num2str(i)+".jpg)"+"{:height='100px' width='100px'}"+"\n");
+    fprintf(file,"![image](/matlab/pics/iter"+num2str(i)+".jpg)"+"{:height='50%' width='50%'}"+"\n");
 end
 fclose(file);
 
