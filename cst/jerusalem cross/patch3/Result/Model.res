0d0686d18dde9579fb63e5da5c81bd00d0657356
RohitD <rohitradar@gmail.com>
2021-12-31 23:18:48 +0530

    added aluminum

---
+++ cst/jerusalem cross/patch3/Result/Model.res
@@ -0,0 +1,181 @@
+MWS Result File Version 20150206
+size=i:18
+
+type=s:FOLDER
+problemclass=s::8:1000
+visibility=s:visible
+creation=s:internal
+lifetime=s:persistent
+result=s:0
+treepath=s:1D Results
+
+type=s:XYSIGNAL2
+subtype=s:user
+problemclass=s:Low Frequency:4:3
+visibility=s:visible
+creation=s:internal
+lifetime=s:persistent
+result=s:0
+treepath=s:Excitation Signals\default
+files=s:signal_default_lf.sig
+
+type=s:XYSIGNAL2
+subtype=s:user
+problemclass=s:High Frequency:0:0
+visibility=s:visible
+creation=s:internal
+lifetime=s:persistent
+result=s:0
+treepath=s:Excitation Signals\default
+files=s:signal_default.sig
+
+type=s:XYSIGNAL2
+subtype=s:user
+problemclass=s::6:1000
+visibility=s:visible
+creation=s:internal
+lifetime=s:persistent
+result=s:0
+treepath=s:1D Results\Materials\Aluminum\Particles\SEY\Elastic
+files=s:Aluminum_SEY_Elastic.sig
+
+type=s:XYSIGNAL2
+subtype=s:user
+problemclass=s::6:1000
+visibility=s:visible
+creation=s:internal
+lifetime=s:persistent
+result=s:0
+treepath=s:1D Results\Materials\Aluminum\Particles\SEY\Rediff
+files=s:Aluminum_SEY_Rediff.sig
+
+type=s:XYSIGNAL2
+subtype=s:user
+problemclass=s::6:1000
+visibility=s:visible
+creation=s:internal
+lifetime=s:persistent
+result=s:0
+treepath=s:1D Results\Materials\Aluminum\Particles\SEY\True
+files=s:Aluminum_SEY_True.sig
+
+type=s:XYSIGNAL2
+subtype=s:user
+problemclass=s::6:1000
+visibility=s:visible
+creation=s:internal
+lifetime=s:persistent
+result=s:0
+treepath=s:1D Results\Materials\Aluminum\Particles\SEY\Total
+files=s:Aluminum_SEY_Total.sig
+
+type=s:XYSIGNAL2
+subtype=s:user
+problemclass=s::6:1000
+visibility=s:visible
+creation=s:internal
+lifetime=s:persistent
+result=s:0
+treepath=s:1D Results\Materials\Aluminum\Particles\PDF\Elastic
+files=s:Aluminum_PDF_Elastic.sig
+
+type=s:XYSIGNAL2
+subtype=s:user
+problemclass=s::6:1000
+visibility=s:visible
+creation=s:internal
+lifetime=s:persistent
+result=s:0
+treepath=s:1D Results\Materials\Aluminum\Particles\PDF\Rediff
+files=s:Aluminum_PDF_Rediff.sig
+
+type=s:XYSIGNAL2
+subtype=s:user
+problemclass=s::6:1000
+visibility=s:visible
+creation=s:internal
+lifetime=s:persistent
+result=s:0
+treepath=s:1D Results\Materials\Aluminum\Particles\PDF\True
+files=s:Aluminum_PDF_True.sig
+
+type=s:XYSIGNAL2
+subtype=s:user
+problemclass=s::6:1000
+visibility=s:visible
+creation=s:internal
+lifetime=s:persistent
+result=s:0
+treepath=s:1D Results\Materials\Aluminum\Particles\PDF\Total
+files=s:Aluminum_PDF_Total.sig
+
+type=s:XYSIGNAL2
+subtype=s:user
+problemclass=s::8:1000
+visibility=s:visible
+creation=s:internal
+lifetime=s:persistent
+result=s:1
+treepath=s:1D Results\Materials\FR-4 (lossy)\Dispersive\Eps' (Fit)
+files=s:FR-4 (lossy)_eps_re.sig
+
+type=s:XYSIGNAL2
+subtype=s:user
+problemclass=s::8:1000
+visibility=s:visible
+creation=s:internal
+lifetime=s:persistent
+result=s:1
+treepath=s:1D Results\Materials\FR-4 (lossy)\Dispersive\Eps'' (Fit)
+files=s:FR-4 (lossy)_eps_im.sig
+
+type=s:XYSIGNAL2
+subtype=s:user
+problemclass=s::8:1000
+visibility=s:visible
+creation=s:internal
+lifetime=s:persistent
+result=s:1
+treepath=s:1D Results\Materials\FR-4 (lossy)\Dispersive\Eps TangD (Fit)
+files=s:FR-4 (lossy)_eps_tgd.sig
+
+type=s:XYSIGNAL2
+subtype=s:user
+problemclass=s::8:1000
+visibility=s:visible
+creation=s:internal
+lifetime=s:persistent
+result=s:1
+treepath=s:1D Results\Materials\Aluminum\Surface Impedance\Z' (Fit)
+files=s:Aluminum_Z_re.sig
+
+type=s:XYSIGNAL2
+subtype=s:user
+problemclass=s::8:1000
+visibility=s:visible
+creation=s:internal
+lifetime=s:persistent
+result=s:1
+treepath=s:1D Results\Materials\Aluminum\Surface Impedance\Z'' (Fit)
+files=s:Aluminum_Z_im.sig
+
+type=s:XYSIGNAL2
+subtype=s:user
+problemclass=s::8:1000
+visibility=s:visible
+creation=s:internal
+lifetime=s:persistent
+result=s:1
+treepath=s:1D Results\Materials\Aluminum\Surface Impedance\Z' (Theory)
+files=s:Aluminum_Z_datalist_re.sig
+
+type=s:XYSIGNAL2
+subtype=s:user
+problemclass=s::8:1000
+visibility=s:visible
+creation=s:internal
+lifetime=s:persistent
+result=s:1
+treepath=s:1D Results\Materials\Aluminum\Surface Impedance\Z'' (Theory)
+files=s:Aluminum_Z_datalist_im.sig
+
---
+++ cst/jerusalem cross/try3.m
@@ -0,0 +1,145 @@
+clc;
+clear all;
+
+addpath(genpath('E:\Desktop\Now\rcs\cst\CST-MATLAB-API-master'));
+cst = actxserver('CSTStudio.application');
+mws = cst.invoke('NewMWS');
+CstDefaultUnits(mws)
+CstDefineFrequencyRange(mws,1,15)
+CstMeshInitiator(mws)
+
+Xmin='expanded open';
+Xmax='expanded open';
+Ymin='expanded open';
+Ymax='expanded open';
+Zmin='expanded open';
+Zmax='expanded open';
+minfrequency = 1;
+CstDefineOpenBoundary(mws,minfrequency,Xmin,Xmax,Ymin,Ymax,Zmin,Zmax)
+
+XminSpace = 0;
+XmaxSpace = 0;
+YminSpace = 0;
+YmaxSpace = 0;
+ZminSpace = 0;
+ZmaxSpace = 0;
+CstDefineBackroundMaterial(mws,XminSpace,XmaxSpace, YminSpace, YmaxSpace, ZminSpace, ZmaxSpace)
+
+CstFR4lossy(mws)
+CstAluminum(mws)
+
+wg = 300;
+lg = 300;
+hg = 1;
+hv = 5;
+hs = 1.57;
+hp = 1;
+w = 0.5;
+pw = 4;
+t = 0.5;
+N=20;
+M=20;
+interx = wg/M;
+intery = lg/N;
+
+file = load('E:\Desktop\Now\rcs\matlab\phase\iter2421.mat');
+phase = file.phasesave;
+file = readmatrix('8ghz_data.csv');
+pl = ones(N,M);
+x = file(:,2);
+for i=1:1:N
+    for j=1:1:M
+        [val,index]=min(abs(x-phase(i,j)));
+        pl(i,j)=file(index,1);
+    end
+end
+
+Name = 'GroundPlane';
+component = 'component';
+material = 'Aluminum';
+Xrange = [-wg/2 wg/2];
+Yrange = [-lg/2 lg/2];
+Zrange = [0 hg];
+Cstbrick(mws, Name, component, material, Xrange, Yrange, Zrange)
+Name = 'Vacuum';
+component = 'component';
+material = 'Vacuum';
+Xrange = [-wg/2 wg/2];
+Yrange = [-lg/2 lg/2];
+Zrange = [hg hg+hv];
+Cstbrick(mws, Name, component, material, Xrange, Yrange, Zrange)
+Name = 'Substrate';
+component = 'component';
+material = 'FR-4 (lossy)';
+Xrange = [-wg/2 wg/2];
+Yrange = [-lg/2 lg/2];
+Zrange = [hg+hv hg+hv+hs];
+Cstbrick(mws, Name, component, material, Xrange, Yrange, Zrange)
+
+for i=1:1:N
+    for j=1:1:M
+        PL = pl(i,j);
+        dispx=(-wg/2+interx/2) + interx*(j-1);
+        dispy=(lg/2-intery/2) - intery*(i-1);
+        Name = ['vert' num2str(i) 'x' num2str(j)];
+        component = 'component';
+        material = 'Aluminum';
+        Xrange = dispx+[-w/2 w/2];
+        Yrange = dispy+[-PL/2 PL/2];
+        Zrange = [hg+hv+hs hg++hv+hs+hp];
+        Cstbrick(mws, Name, component, material, Xrange, Yrange, Zrange)
+        Name = ['hor' num2str(i) 'x' num2str(j)];
+        component = 'component';
+        material = 'Aluminum';
+        Xrange = dispx+[-PL/2 PL/2];
+        Yrange = dispy+[-w/2 w/2];
+        Zrange = [hg+hv+hs hg++hv+hs+hp];
+        Cstbrick(mws, Name, component, material, Xrange, Yrange, Zrange)
+        %{
+        Name = ['top' num2str(i) 'x' num2str(j)];
+        component = 'component';
+        material = 'Aluminum';
+        Xrange = dispx+[-pw/2 pw/2];
+        Yrange = dispy+[PL/2 PL/2+t];
+        Zrange = [hg+hv+hs hg++hv+hs+hp];
+        Cstbrick(mws, Name, component, material, Xrange, Yrange, Zrange)
+        Name = ['down' num2str(i) 'x' num2str(j)];
+        component = 'component';
+        material = 'Aluminum';
+        Xrange = dispx+[-pw/2 pw/2];
+        Yrange = dispy+[-PL/2 -PL/2-t];
+        Zrange = [hg+hv+hs hg++hv+hs+hp];
+        Cstbrick(mws, Name, component, material, Xrange, Yrange, Zrange)
+        Name = ['left' num2str(i) 'x' num2str(j)];
+        component = 'component';
+        material = 'Aluminum';
+        Xrange = dispx+[-PL/2-t -PL/2];
+        Yrange = dispy+[-pw/2 pw/2];
+        Zrange = [hg+hv+hs hg++hv+hs+hp];
+        Cstbrick(mws, Name, component, material, Xrange, Yrange, Zrange)
+        Name = ['right' num2str(i) 'x' num2str(j)];
+        component = 'component';
+        material = 'Aluminum';
+        Xrange = dispx+[PL/2 PL/2+t];
+        Yrange = dispy+[-pw/2 pw/2];
+        Zrange = [hg+hv+hs hg++hv+hs+hp];
+        Cstbrick(mws, Name, component, material, Xrange, Yrange, Zrange)
+        %}
+        vert=['component:vert' num2str(i) 'x' num2str(j)];
+        hor=['component:hor' num2str(i) 'x' num2str(j)];
+        CstAdd(mws,vert,hor);
+        %{
+        top=['component:top' num2str(i) 'x' num2str(j)];
+        down=['component:down' num2str(i) 'x' num2str(j)];
+        left=['component:left' num2str(i) 'x' num2str(j)];
+        right=['component:right' num2str(i) 'x' num2str(j)];
+        CstAdd(mws,hor,right);
+        CstAdd(mws,hor,left);
+        CstAdd(mws,vert,top);
+        CstAdd(mws,vert,down);
+        %}
+    end
+end
+
+location = "E:\Desktop\Now\rcs\cst\jerusalem cross\patch3.cst";
+CstSaveProject(mws,location);
